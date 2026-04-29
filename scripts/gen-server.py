7613223dec14c4e2fb35ee3b918fd63ac0b3e0b2
Femi <ajuwonfemi121@gmail.com>
2026-04-29 12:38:31 +0100

    dummy commit

--- scripts/gen-server.py
+++ scripts/gen-server.py
@@ -15,13 +15,12 @@ def generate():
     # 2. Start building the Express file
     code = [
         'import express from "express";',
-        'import * as Controllers from "./controllers.js";',
+        'import * as APIs from "./test-api.js";',
         'import * as Models from "../generated-api/models/index.js";',
         '',
         'const app = express();',
         'app.use(express.json());',
         'const PORT = 3000;',
-        ''
     ]
 
     # 3. Process Paths
@@ -44,8 +43,9 @@ def generate():
             
             summary = details.get('summary', op_id)
             
+            code.append("")
             code.append(f'// {summary}')
-            code.append(f'app.{method.lower()}("{express_path}", Controllers.{op_id});')
+            code.append(f'app.{method.lower()}("{express_path}", APIs.{op_id});')
 
     # 4. Finalize
     code.append('\napp.listen(PORT, () => {')
@@ -53,10 +53,13 @@ def generate():
     code.append('});')
 
     # 5. Write to file
-    os.makedirs('src', exist_ok=True)
-    with open('src/server.ts', 'w') as f:
+    dir = 'src/server'
+    os.makedirs(dir, exist_ok=True)
+    os.chdir(dir)
+    server_file = "app-server.ts"
+    with open(server_file, 'w') as f:
         f.write("\n".join(code))
-    print("Successfully generated src/server.ts")
+    print(f"Successfully generated {server_file}")
 
 if __name__ == "__main__":
     generate()
